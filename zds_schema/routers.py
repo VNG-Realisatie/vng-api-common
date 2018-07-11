d87bfff8f665d2c11541af42ed0f2110a96cc792
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-11 18:29:45 +0200

    Mixin for more declarative nested router configuration

--- zds_schema/routers.py
+++ zds_schema/routers.py
@@ -1,9 +1,8 @@
 from rest_framework_nested import routers
-from rest_framework_nested.routers import NestedSimpleRouter  # noqa
 
 
-class DefaultRouter(routers.DefaultRouter):
-    __nested_router = None
+class ZDSNestedRegisteringMixin:
+    _nested_router = None
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('trailing_slash', False)
@@ -11,32 +10,46 @@ class DefaultRouter(routers.DefaultRouter):
 
     def get_urls(self):
         urls = super().get_urls()
-        nested_router = self.__nested_router
+        nested_router = self._nested_router
         if nested_router is not None:
             urls += nested_router.urls
         return urls
 
     def register(self, prefix, viewset, nested=None, *args, **kwargs):
-        base_name = kwargs.pop('base_name', self.get_default_base_name(viewset))
-
         super().register(prefix, viewset, *args, **kwargs)
 
         if not nested:
             return
 
-        nested_router = routers.NestedSimpleRouter(
+        base_name = kwargs.get('base_name', self.get_default_base_name(viewset))
+
+        self._nested_router = NestedSimpleRouter(
             self, prefix,
             lookup=base_name, trailing_slash=False
         )
-
         for _nested in nested:
-            nested_router.register(_nested.prefix, _nested.viewset, **_nested.kwargs)
+            self._nested_router.register(
+                _nested.prefix, _nested.viewset,
+                _nested.nested, **_nested.kwargs
+            )
 
-        self.__nested_router = nested_router
+
+class NestedSimpleRouter(ZDSNestedRegisteringMixin, routers.NestedSimpleRouter):
+    pass
+
+
+class DefaultRouter(ZDSNestedRegisteringMixin, routers.DefaultRouter):
+    pass
 
 
 class nested:
-    def __init__(self, prefix, viewset, **kwargs):
+    def __init__(self, prefix, viewset, nested=None, **kwargs):
         self.prefix = prefix
         self.viewset = viewset
+        self.nested = nested
         self.kwargs = kwargs
+
+    def __repr__(self):
+        return "nested(prefix={!r}, viewset={!r}".format(
+            self.prefix, self.viewset
+        )
