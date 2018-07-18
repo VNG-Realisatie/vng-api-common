34a74c78b9e35579d9f2fd6860503e8f01b0088b
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-18 18:16:05 +0200

    Beating search into Swagger

--- zds_schema/schema.py
+++ zds_schema/schema.py
@@ -15,3 +15,10 @@ class AutoSchema(SwaggerAutoSchema):
         action = operation_keys[-1]
         model_name = self.model._meta.model_name
         return f"{model_name}_{action}"
+
+    def get_request_serializer(self):
+        if hasattr(self.view, 'action'):
+            action = getattr(self.view, self.view.action)
+            if getattr(action, 'is_search_action', False):
+                return self.view.get_search_input_serializer_class()()
+        return super().get_request_serializer()
---
+++ zds_schema/search.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class SearchMixin:
+    search_input_serializer_class = None
+
+    def get_search_input_serializer_class(self):
+        return self.search_input_serializer_class or self.serializer_class
+
+    def get_search_input(self):
+        serializer = self.get_search_input_serializer_class()(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.validated_data
+
+    def get_search_output(self, queryset: models.QuerySet):
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return serializer.data
