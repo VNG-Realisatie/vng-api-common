5c76a982864a0784e902378ca60d7fd1c651a3c7
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-19 10:02:16 +0200

    Return correct search responses when inspecting schema

--- zds_schema/schema.py
+++ zds_schema/schema.py
@@ -1,4 +1,8 @@
+from collections import OrderedDict
+
+from drf_yasg import openapi
 from drf_yasg.inspectors import SwaggerAutoSchema
+from rest_framework import status
 
 
 class AutoSchema(SwaggerAutoSchema):
@@ -8,6 +12,13 @@ class AutoSchema(SwaggerAutoSchema):
         qs = self.view.get_queryset()
         return qs.model
 
+    @property
+    def _is_search_view(self):
+        if not hasattr(self.view, 'action'):
+            return
+        action = getattr(self.view, self.view.action)
+        return getattr(action, 'is_search_action', False)
+
     def get_operation_id(self, operation_keys):
         """
         Simply return the model name as lowercase string, postfixed with the operation name.
@@ -16,9 +27,22 @@ class AutoSchema(SwaggerAutoSchema):
         model_name = self.model._meta.model_name
         return f"{model_name}_{action}"
 
+    def should_page(self):
+        if self._is_search_view:
+            return hasattr(self.view, 'paginator')
+        return super().should_page()
+
     def get_request_serializer(self):
-        if hasattr(self.view, 'action'):
-            action = getattr(self.view, self.view.action)
-            if getattr(action, 'is_search_action', False):
-                return self.view.get_search_input_serializer_class()()
+        if self._is_search_view:
+            return self.view.get_search_input_serializer_class()()
         return super().get_request_serializer()
+
+    def get_default_responses(self):
+        if self._is_search_view:
+            response_status = status.HTTP_200_OK
+            response_schema = self.serializer_to_schema(self.get_view_serializer())
+            schema = openapi.Schema(type=openapi.TYPE_ARRAY, items=response_schema)
+            if self.should_page():
+                schema = self.get_paginated_response(schema) or schema
+            return OrderedDict({str(response_status): schema})
+        return super().get_default_responses()
