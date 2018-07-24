d54a5ef7619a32999fb0283c2a71c6b20e0558aa
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-24 16:23:49 +0200

    More UUID introspection

--- zds_schema/management/commands/generate_swagger.py
+++ zds_schema/management/commands/generate_swagger.py
@@ -1,5 +1,15 @@
+import logging
+import os
+
+from django.contrib.auth.models import User
+from django.core.exceptions import ImproperlyConfigured
+
+from drf_yasg import openapi
+from drf_yasg.app_settings import swagger_settings
 from drf_yasg.management.commands import generate_swagger
 from rest_framework.settings import api_settings
+
+from ...schema import OpenAPISchemaGenerator
 
 
 class Command(generate_swagger.Command):
@@ -16,3 +26,48 @@ class Command(generate_swagger.Command):
         del schema.host
         del schema.schemes
         super().write_schema(schema, stream, format)
+
+    # need to overwrite the generator class...
+    def handle(self, output_file, overwrite, format, api_url, mock, user, private, *args, **options):
+        # disable logs of WARNING and below
+        logging.disable(logging.WARNING)
+
+        info = getattr(swagger_settings, 'DEFAULT_INFO', None)
+        if not isinstance(info, openapi.Info):
+            raise ImproperlyConfigured(
+                'settings.SWAGGER_SETTINGS["DEFAULT_INFO"] should be an '
+                'import string pointing to an openapi.Info object'
+            )
+
+        if not format:
+            if os.path.splitext(output_file)[1] in ('.yml', '.yaml'):
+                format = 'yaml'
+        format = format or 'json'
+
+        api_url = api_url or swagger_settings.DEFAULT_API_URL
+
+        user = User.objects.get(username=user) if user else None
+        mock = mock or private or (user is not None)
+        if mock and not api_url:
+            raise ImproperlyConfigured(
+                '--mock-request requires an API url; either provide '
+                'the --url argument or set the DEFAULT_API_URL setting'
+            )
+
+        request = self.get_mock_request(api_url, format, user) if mock else None
+
+        generator = OpenAPISchemaGenerator(
+            info=info,
+            url=api_url
+        )
+        schema = generator.get_schema(request=request, public=not private)
+
+        if output_file == '-':
+            self.write_schema(schema, self.stdout, format)
+        else:
+            # normally this would be easily done with open(mode='x'/'w'),
+            # but python 2 is a pain in the ass as usual
+            flags = os.O_CREAT | os.O_WRONLY
+            flags = flags | (os.O_TRUNC if overwrite else os.O_EXCL)
+            with os.fdopen(os.open(output_file, flags), "w") as stream:
+                self.write_schema(schema, stream, format)
--- zds_schema/schema.py
+++ zds_schema/schema.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
 
 from drf_yasg import openapi
+from drf_yasg.generators import (
+    OpenAPISchemaGenerator as _OpenAPISchemaGenerator
+)
 from drf_yasg.inspectors import SwaggerAutoSchema
 from rest_framework import serializers, status
 
@@ -107,3 +110,25 @@ class AutoSchema(SwaggerAutoSchema):
             serializer, {'field_inspectors': self.field_inspectors}
         ) or []
         return base + extra
+
+
+class OpenAPISchemaGenerator(_OpenAPISchemaGenerator):
+
+    def get_path_parameters(self, path, view_cls):
+        """Return a list of Parameter instances corresponding to any templated path variables.
+
+        :param str path: templated request path
+        :param type view_cls: the view class associated with the path
+        :return: path parameters
+        :rtype: list[openapi.Parameter]
+        """
+        parameters = super().get_path_parameters(path, view_cls)
+
+        # see if we can specify UUID a bit more
+        for parameter in parameters:
+            # the most pragmatic of checks
+            if not parameter.name.endswith('_uuid'):
+                continue
+            parameter.format = openapi.FORMAT_UUID
+            parameter.description = "Unieke resource identifier (UUID4)"
+        return parameters
