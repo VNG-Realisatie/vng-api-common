b4a327b651ea1b31c000964e896dd1df8d52b38d
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-30 14:53:45 +0200

    Consistent vastleggen van taalopties

--- zds_schema/fields.py
+++ zds_schema/fields.py
@@ -2,8 +2,16 @@ from django.core import checks
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from iso639 import languages
+
 from .constants import BSN_LENGTH, RSIN_LENGTH
 from .validators import validate_rsin
+
+ISO_639_2B = languages.part2b
+
+LANGUAGE_CHOICES = tuple([
+    (code, language.name) for code, language in ISO_639_2B.items()
+])
 
 
 class RSINField(models.CharField):
@@ -51,6 +59,41 @@ class BSNField(models.CharField):
                     "BSNField may not override 'max_length' attribute.",
                     obj=self,
                     id='zds_schema.fields.E002',
+                )
+            ]
+        return []
+
+
+class LanguageField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 3)
+        kwargs.setdefault('choices', LANGUAGE_CHOICES)
+        super().__init__(*args, **kwargs)
+
+    def check(self, **kwargs):
+        errors = super().check(**kwargs)
+        errors.extend(self._check_fixed_max_length_attribute(**kwargs))
+        errors.extend(self._check_choices(**kwargs))
+        return errors
+
+    def _check_fixed_max_length_attribute(self, **kwargs):
+        if self.max_length != 3:
+            return [
+                checks.Error(
+                    "LanguageField may not override 'max_length' attribute.",
+                    obj=self,
+                    id='zds_schema.fields.E003',
+                )
+            ]
+        return []
+
+    def _check_choices(self, **kwargs):
+        if self.choices != LANGUAGE_CHOICES:
+            return [
+                checks.Error(
+                    "LanguageField may not override 'choices' attribute.",
+                    obj=self,
+                    id='zds_schema.fields.E004',
                 )
             ]
         return []
