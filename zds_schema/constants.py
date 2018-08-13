2922d28dbe73fb3bf1797787e0e0e13e93a3fe7d
Sergei Maertens <sergei@maykinmedia.nl>
2018-08-13 10:15:35 +0200

    Moved constants to shared code

--- zds_schema/constants.py
+++ zds_schema/constants.py
@@ -13,3 +13,25 @@ class VertrouwelijkheidsAanduiding(DjangoChoices):
     confidentieel = ChoiceItem('confidentieel', 'CONFIDENTIEEL')
     geheim = ChoiceItem('geheim', 'GEHEIM')
     zeer_geheim = ChoiceItem('zeer geheim', 'ZEER GEHEIM')
+
+
+class RolOmschrijving(DjangoChoices):
+    adviseur = ChoiceItem('Adviseur', 'Adviseur')
+    behandelaar = ChoiceItem('Behandelaar', 'Behandelaar')
+    belanghebbende = ChoiceItem('Belanghebbende', 'Belanghebbende')
+    beslisser = ChoiceItem('Beslisser', 'Beslisser')
+    initiator = ChoiceItem('Initiator', 'Initiator')
+    klantcontacter = ChoiceItem('Klantcontacter', 'Klantcontacter')
+    zaakcoordinator = ChoiceItem('Zaakcoördinator', 'Zaakcoördinator')
+
+
+class RolOmschrijvingGeneriek(RolOmschrijving):
+    medeinitiator = ChoiceItem('Mede-initiator', 'Mede-initiator')
+
+
+class RolTypes(DjangoChoices):
+    natuurlijk_persoon = ChoiceItem('Natuurlijk persoon', "Natuurlijk persoon")
+    niet_natuurlijk_persoon = ChoiceItem('Niet-natuurlijk persoon', "Niet-natuurlijk persoon")
+    vestiging = ChoiceItem('Vestiging', "Vestiging")
+    organisatorische_eenheid = ChoiceItem('Organisatorische eenheid', "Organisatorische eenheid")
+    medewerker = ChoiceItem('Medewerker', "Medewerker")
