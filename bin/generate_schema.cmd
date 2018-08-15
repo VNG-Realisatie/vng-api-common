e49449d3306bd5b04659057ac7d5908b9040a74c
Joeri Bekker <joeri@maykinmedia.nl>
2018-08-15 13:11:42 +0200

    Added Windows version of generate_schema script.

---
+++ bin/generate_schema.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+REM Run this script from the root of the repository
+
+if "%VIRTUAL_ENV%"=="" (
+    echo You need to activate your virtual env before running this script
+    goto :eof
+)
+
+echo Generating Swagger schema
+python src\manage.py generate_swagger^
+    ./src/swagger2.0.json^
+    --overwrite^
+    --format=json^
+    --mock-request^
+    --url https://example.com/api/v1
+
+echo Converting Swagger to OpenAPI 3.0...
+npm run convert
