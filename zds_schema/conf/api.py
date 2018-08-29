ee45d8488338e0d0a34e25768e4f31c748573f69
Sergei Maertens <sergei@maykinmedia.nl>
2018-08-29 17:31:57 +0200

    Centralize common API settings + exception handler

---
+++ zds_schema/conf/api.py
@@ -0,0 +1,78 @@
+BASE_REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    #     # 'rest_framework.authentication.SessionAuthentication',
+    #     # 'rest_framework.authentication.BasicAuthentication'
+    # ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'oauth2_provider.contrib.rest_framework.TokenHasReadWriteScope',
+    #     # 'rest_framework.permissions.IsAuthenticated',
+    #     # 'rest_framework.permissions.AllowAny',
+    # ),
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+    #
+    # # Generic view behavior
+    # 'DEFAULT_PAGINATION_CLASS': 'ztc.api.utils.pagination.HALPagination',
+    'DEFAULT_FILTER_BACKENDS': (
+        'zds_schema.filters.Backend',
+        # 'rest_framework.filters.SearchFilter',
+        # 'rest_framework.filters.OrderingFilter',
+    ),
+    #
+    # # Filtering
+    # 'SEARCH_PARAM': 'zoek',  # 'search',
+    # 'ORDERING_PARAM': 'sorteer',  # 'ordering',
+    #
+    # Versioning
+    'DEFAULT_VERSION': '1',
+    'ALLOWED_VERSIONS': ('1', ),
+    'VERSION_PARAM': 'version',
+    #
+    # # Exception handling
+    'EXCEPTION_HANDLER': 'zds_schema.views.exception_handler',
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
+
+BASE_SWAGGER_SETTINGS = {
+    # 'SECURITY_DEFINITIONS': {
+    #     'OAuth2': {
+    #         'type': 'oauth2',
+    #         'flow': 'application',
+    #         'tokenUrl': '/oauth2/token/',
+    #         'scopes': {
+    #             'write': 'Schrijftoegang tot de catalogus en gerelateerde objecten.',
+    #             'read': 'Leestoegang tot de catalogus en gerelateerde objecten.'
+    #         }
+    #     },
+    #     'Bearer': {
+    #         'type': 'apiKey',
+    #         'name': 'Authorization',
+    #         'in': 'header'
+    #     },
+    # },
+    'DEFAULT_AUTO_SCHEMA_CLASS': 'zds_schema.schema.AutoSchema',
+    'DEFAULT_INFO': 'must.be.overridden',
+    'DEFAULT_FIELD_INSPECTORS': (
+        'zds_schema.inspectors.geojson.GeometryFieldInspector',
+
+        'drf_yasg.inspectors.CamelCaseJSONFilter',
+        'drf_yasg.inspectors.RecursiveFieldInspector',
+        'drf_yasg.inspectors.ReferencingSerializerInspector',
+        'drf_yasg.inspectors.ChoiceFieldInspector',
+        'drf_yasg.inspectors.FileFieldInspector',
+        'drf_yasg.inspectors.DictFieldInspector',
+        'drf_yasg.inspectors.HiddenFieldInspector',
+        'drf_yasg.inspectors.RelatedFieldInspector',
+        'drf_yasg.inspectors.SimpleFieldInspector',
+        'drf_yasg.inspectors.StringDefaultFieldInspector',
+    ),
+    'DEFAULT_FILTER_INSPECTORS': (
+        'zds_schema.inspectors.query.FilterInspector',
+    )
+}
---
+++ zds_schema/views.py
@@ -0,0 +1,43 @@
+import logging
+import uuid
+from collections import OrderedDict
+
+from rest_framework.views import exception_handler as drf_exception_handler
+
+logger = logging.getLogger(__name__)
+
+STATUS_TO_TITLE = {}  # TODO
+
+
+def exception_handler(exc, context):
+    """
+    Transform 4xx and 5xx errors into DSO-compliant shape.
+    """
+    response = drf_exception_handler(exc, context)
+    if response is None:
+        return
+
+    data = getattr(response, 'data', {})
+
+    # Get the human-readable title, from most-specific to least specific
+    # because of how `drf_exception_handler` works, this is always an APIException
+    # instance
+    title = STATUS_TO_TITLE.get(response.status_code, exc.default_detail)
+
+    # if we're passing an instance that the client receives, this should be
+    # retrievable somewhere. Ideally, this would be in Sentry, but for now we
+    # can do an attempt to put the UUID in the log files if logging is setup
+    # properly.
+    exc_id = str(uuid.uuid4())
+    logger.exception("Exception %s ocurred", exc_id)
+
+    response.data = OrderedDict([
+        ('type', exc.__class__.__name__),
+        ('title', title),
+        ('status', response.status_code),
+        # re-use DRFs default format to get the detail message
+        ('detail', data.get('detail', '')),
+        ('instance', f"urn:uuid:{exc_id}"),
+    ])
+
+    return response
