dc564f93e8913bd32718ef850d5addaaae4eb6b2
Sergei Maertens <sergei@maykinmedia.nl>
2018-12-27 15:57:32 +0100

    :bug: Prevent migrations infite-trigger

--- zds_schema/fields.py
+++ zds_schema/fields.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from django.core import checks
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ugettext
 
 from iso639 import languages
 
@@ -148,9 +148,10 @@ class DaysDurationField(models.DurationField):
         ]
 
         # add sensible help-text
-        _help_text = _("Specifieer de duur als 'DD 00:00'")
+        _help_text = ugettext("Specifieer de duur als 'DD 00:00'")
         help_text = kwargs.get('help_text', '')
-        kwargs['help_text'] = _help_text if not help_text else f"{help_text} {_help_text}"
+        if not help_text.endswith(_help_text):
+            kwargs['help_text'] = _help_text if not help_text else f"{help_text} {_help_text}"
 
         super().__init__(*args, **kwargs)
 
