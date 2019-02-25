3d8630db351a41c5f647fbcca47c2bc6f8b6d663
Sergei Maertens <sergei@maykinmedia.nl>
2019-02-25 15:51:22 +0100

    :sparkles: Added explicit format: duration to generated OAS

--- zds_schema/apps.py
+++ zds_schema/apps.py
@@ -11,6 +11,12 @@ from . import fields
 from .serializers import DurationField
 
 
+# https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#data-types
+# and https://github.com/OAI/OpenAPI-Specification/issues/845 - format: duration
+# is collected somewhere so there's precedent
+FORMAT_DURATION = 'duration'
+
+
 class ZDSSchemaConfig(AppConfig):
     name = 'zds_schema'
 
@@ -21,16 +27,16 @@ class ZDSSchemaConfig(AppConfig):
 
 def patch_duration_type():
 
-    def _patch(basic_types, _field_cls):
+    def _patch(basic_types, _field_cls, format=None):
         for index, (field_cls, basic_type) in enumerate(basic_types):
             if field_cls is _field_cls:
-                basic_types[index] = (_field_cls, (openapi.TYPE_STRING, None))
+                basic_types[index] = (_field_cls, (openapi.TYPE_STRING, format))
                 break
 
-    _patch(model_field_to_basic_type, models.DurationField)
-    _patch(basic_type_info, models.DurationField)
-    _patch(serializer_field_to_basic_type, serializers.DurationField)
-    _patch(basic_type_info, serializers.DurationField)
+    _patch(model_field_to_basic_type, models.DurationField, FORMAT_DURATION)
+    _patch(basic_type_info, models.DurationField, FORMAT_DURATION)
+    _patch(serializer_field_to_basic_type, serializers.DurationField, FORMAT_DURATION)
+    _patch(basic_type_info, serializers.DurationField, FORMAT_DURATION)
 
 
 def register_serializer_field():
