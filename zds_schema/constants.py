de3f0fdf735e4af03158bd53b91340e58a17c70c
annashamray <anna.shamray@maykinmedia.nl>
2019-02-25 14:58:43 +0100

    add ZaakobjectTypes and BetalingsIndicatie to constants.py

--- zds_schema/constants.py
+++ zds_schema/constants.py
@@ -170,3 +170,16 @@ class BrondatumArchiefprocedureAfleidingswijze(DjangoChoices):
                       "attribuutsoort `Datumkenmerk` wordt vastgelegd welke "
                       "datum-attribuutsoort van het zaakobjecttype het betreft.")
     )
+
+
+class ZaakobjectTypes(DjangoChoices):
+    verblijfs_object = ChoiceItem('VerblijfsObject', 'Verblijfsobject')
+    melding_openbare_ruimte = ChoiceItem('MeldingOpenbareRuimte', "Melding openbare ruimte")
+    avg_inzage_verzoek = ChoiceItem('InzageVerzoek', "Inzage verzoek in het kader van de AVG")
+
+
+class BetalingsIndicatie(DjangoChoices):
+    nvt = ChoiceItem('nvt', _("Er is geen sprake van te betalen, met de zaak gemoeide, kosten."))
+    nog_niet = ChoiceItem('nog_niet', _("De met de zaak gemoeide kosten zijn (nog) niet betaald."))
+    gedeeltelijk = ChoiceItem('gedeeltelijk', _("De met de zaak gemoeide kosten zijn gedeeltelijk betaald."))
+    geheel = ChoiceItem('geheel', _("De met de zaak gemoeide kosten zijn geheel betaald."))
