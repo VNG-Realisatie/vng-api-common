1240dac2c0baac9533ef6db337088ad470512d52
annashamray <anna.shamray@maykinmedia.nl>
2019-03-29 12:45:11 +0100

    add client_id to jwt_payload + new permission

--- vng_api_common/middleware.py
+++ vng_api_common/middleware.py
@@ -76,7 +76,10 @@ class JWTPayload:
                 code='invalid-jwt-signature'
             )
 
-        return payload.get('zds', EMPTY_PAYLOAD)
+        res = payload.get('zds', EMPTY_PAYLOAD)
+        res['client_id'] = payload.get('iss', '')
+
+        return res
 
     def has_scopes(self, scopes: Union[Scope, None]) -> bool:
         """
