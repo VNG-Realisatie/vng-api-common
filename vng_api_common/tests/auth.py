41f2805527176a7f04c57cfd10258114cc1d3acd
annashamray <anna.shamray@maykinmedia.nl>
2019-04-30 17:07:22 +0200

    modify auth permision and add notify handler

--- vng_api_common/tests/auth.py
+++ vng_api_common/tests/auth.py
@@ -8,6 +8,7 @@ from ..authorizations.models import (
     Applicatie, AuthorizationsConfig, Autorisatie
 )
 from ..constants import VertrouwelijkheidsAanduiding
+
 from ..models import JWTSecret
 from ..scopes import Scope
 
