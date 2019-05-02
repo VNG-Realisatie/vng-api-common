b98686cc9588fb08d4da6a244f577619d1a55551
Sergei Maertens <sergei@maykinmedia.nl>
2019-05-02 11:18:25 +0200

    :fire: Remove unused decorator

--- vng_api_common/decorators.py
+++ vng_api_common/decorators.py
@@ -1,14 +1,6 @@
 from typing import Any
 
 from django.db.models.base import ModelBase
-from django.utils.decorators import method_decorator
-
-from drf_yasg.utils import swagger_auto_schema
-
-
-def action_description(action: str, description: str):
-    decorator = swagger_auto_schema(operation_description=description)
-    return method_decorator(name=action, decorator=decorator)
 
 
 def field_default(field: str, default: Any):
