5dc504e2834ef5918b3c3aaebf7905695378b0a1
Sergei Maertens <sergei@maykinmedia.nl>
2019-05-27 13:04:51 +0200

    :white_check_mark: Added tests for partial serialization of gegevensgroepen

---
+++ testapp/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from testapp.models import Person
+
+from vng_api_common.serializers import GegevensGroepSerializer
+
+
+class AddressSerializer(GegevensGroepSerializer):
+    class Meta:
+        model = Person
+        gegevensgroep = 'address'
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+
+    class Meta:
+        model = Person
+        fields = ("address", "name")
