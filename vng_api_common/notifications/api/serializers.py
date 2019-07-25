9c575465cb065f58f18224bccb00cea1a212623e
Sergei Maertens <sergei@maykinmedia.nl>
2019-07-25 14:37:34 +0200

    :bug: Notificatie.aanmaakdatum may not be in the future

--- vng_api_common/notifications/api/serializers.py
+++ vng_api_common/notifications/api/serializers.py
@@ -2,6 +2,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
+from ...validators import UntilNowValidator
+
 
 class NotificatieSerializer(serializers.Serializer):
     kanaal = serializers.CharField(label=_("kanaal"), max_length=50)
@@ -9,7 +11,7 @@ class NotificatieSerializer(serializers.Serializer):
     resource = serializers.CharField(label=_("resource"), max_length=100)
     resource_url = serializers.URLField(label=_("URL naar de resource waarover de notificatie gaat"))
     actie = serializers.CharField(label=_("actie"), max_length=100)
-    aanmaakdatum = serializers.DateTimeField(label=_("aanmaakdatum"))
+    aanmaakdatum = serializers.DateTimeField(label=_("aanmaakdatum"), validators=[UntilNowValidator()])
     kenmerken = serializers.DictField(
         child=serializers.CharField(max_length=1000),
         required=False
