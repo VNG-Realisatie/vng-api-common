cf01dd25f273e9284031e9b938477657731493e0
Sergei Maertens <sergei@maykinmedia.nl>
2019-08-01 12:28:31 +0200

    :art: Use multiline string

--- vng_api_common/notifications/utils.py
+++ vng_api_common/notifications/utils.py
@@ -6,11 +6,14 @@ def notification_documentation(kanaal: Kanaal):
     Generate notification documentation for an OpenAPI specification containing
     the relevant resources and actions for a given KANAAL
     """
-    doc = f"""### Notificaties \
-           \n\nDeze API publiceert notificaties op het kanaal `{kanaal.label}`. \
-           \n\n{kanaal.description}
-           \n\n**Resources en acties**\n\n"""
+    doc = f"""### Notificaties
 
+Deze API publiceert notificaties op het kanaal `{kanaal.label}`.
+
+{kanaal.description}
+
+**Resources en acties**
+"""
     for resource, actions in kanaal.get_usage():
         doc += f"""- `{resource}`: {', '.join(actions)}\n"""
     return doc
