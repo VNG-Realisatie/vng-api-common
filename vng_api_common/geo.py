cf16dfb8c0933457bdcd120000096684e5f60d1c
Steven Bal <steven@maykinmedia.nl>
2019-09-05 12:35:01 +0200

    Stop checking for geo headers in case of destroy

--- vng_api_common/geo.py
+++ vng_api_common/geo.py
@@ -43,6 +43,9 @@ class GeoMixin:
             if content_crs != DEFAULT_CRS:
                 raise NotAcceptable(detail=f"CRS '{content_crs}' is niet ondersteund")
 
+        if self.action == "destroy":
+            return
+
         # client must indicate which CRS they want in the response
         requested_crs = extract_header(request, HEADER_ACCEPT)
         if requested_crs is None:
