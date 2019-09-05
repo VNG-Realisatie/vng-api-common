c086932ff3c692b5375d0adcf10cd851f010da4f
annashamray <anna.shamray@maykinmedia.nl>
2019-09-05 17:05:11 +0200

    black

--- vng_api_common/templatetags/vng_api_common.py
+++ vng_api_common/templatetags/vng_api_common.py
@@ -43,8 +43,8 @@ def gemmaonline_url(resource: str):
 @register.filter
 @stringfilter
 def is_local(host: str):
-    local_hosts = ['localhost', '127.0.0.1']
-    hostname = host.rsplit(':')[0]
+    local_hosts = ["localhost", "127.0.0.1"]
+    hostname = host.rsplit(":")[0]
     if hostname in local_hosts:
         return True
     return False
