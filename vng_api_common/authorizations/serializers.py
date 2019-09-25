ca7b96772728ddabe047124add07287199504b67
Sergei Maertens <sergei@maykinmedia.nl>
2019-09-25 15:10:20 +0200

    :bug: Clear autorisaties if empty list is provided

--- vng_api_common/authorizations/serializers.py
+++ vng_api_common/authorizations/serializers.py
@@ -16,19 +16,14 @@ logger = logging.getLogger(__name__)
 
 class AutorisatieBaseSerializer(PolymorphicSerializer):
     discriminator = Discriminator(
-        discriminator_field='component',
+        discriminator_field="component",
         mapping={
-            ComponentTypes.zrc: (
-                'zaaktype',
-                'max_vertrouwelijkheidaanduiding',
-            ),
+            ComponentTypes.zrc: ("zaaktype", "max_vertrouwelijkheidaanduiding"),
             ComponentTypes.drc: (
-                'informatieobjecttype',
-                'max_vertrouwelijkheidaanduiding',
+                "informatieobjecttype",
+                "max_vertrouwelijkheidaanduiding",
             ),
-            ComponentTypes.brc: (
-                'besluittype',
-            ),
+            ComponentTypes.brc: ("besluittype",),
             ComponentTypes.brc: ("besluittype",),
             ComponentTypes.nrc: (),
             ComponentTypes.ztc: (),
@@ -44,18 +39,14 @@ class AutorisatieBaseSerializer(PolymorphicSerializer):
 
     class Meta:
         model = Autorisatie
-        fields = (
-            'component',
-            'component_weergave',
-            'scopes',
-        )
+        fields = ("component", "component_weergave", "scopes")
         extra_kwargs = {
-            'scopes': {
-                'help_text': _(
+            "scopes": {
+                "help_text": _(
                     "Lijst van scope labels. Elke scope geeft toegang tot een "
                     "set van acties/operaties, zoals gedocumenteerd bij de "
                     "betreffende component."
-                ),
+                )
             }
         }
         validators = [AutorisatieValidator()]
@@ -64,7 +55,7 @@ class AutorisatieBaseSerializer(PolymorphicSerializer):
         super().__init__(*args, **kwargs)
 
         value_display_mapping = add_choice_values_help_text(ComponentTypes)
-        self.fields['component'].help_text += f"\n\n{value_display_mapping}"
+        self.fields["component"].help_text += f"\n\n{value_display_mapping}"
 
 
 class ApplicatieSerializer(serializers.HyperlinkedModelSerializer):
@@ -73,24 +64,22 @@ class ApplicatieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Applicatie
         fields = (
-            'url',
-            'client_ids',
-            'label',
-            'heeft_alle_autorisaties',
-            'autorisaties'
+            "url",
+            "client_ids",
+            "label",
+            "heeft_alle_autorisaties",
+            "autorisaties",
         )
         extra_kwargs = {
-            'url': {
-                'lookup_field': 'uuid',
+            "url": {"lookup_field": "uuid"},
+            "heeft_alle_autorisaties": {"required": False},
+            "client_ids": {
+                "validators": [UniqueClientIDValidator()],
+                "help_text": _(
+                    "Lijst van consumer identifiers (hun 'client_id'). Een "
+                    "`client_id` mag slechts bij één applicatie-object voorkomen."
+                ),
             },
-            'heeft_alle_autorisaties': {
-                'required': False,
-            },
-            'client_ids': {
-                'validators': [UniqueClientIDValidator()],
-                'help_text': _("Lijst van consumer identifiers (hun 'client_id'). Een "
-                               "`client_id` mag slechts bij één applicatie-object voorkomen."),
-            }
         }
 
     def validate(self, attrs):
@@ -104,24 +93,28 @@ class ApplicatieSerializer(serializers.HyperlinkedModelSerializer):
             autorisaties_obj = self.instance.autorisaties.all()
             heeft_alle_autorisaties_obj = self.instance.heeft_alle_autorisaties
 
-        autorisaties = validated_attrs.get('autorisaties', autorisaties_obj)
-        heeft_alle_autorisaties = validated_attrs.get('heeft_alle_autorisaties', heeft_alle_autorisaties_obj)
+        autorisaties = validated_attrs.get("autorisaties", autorisaties_obj)
+        heeft_alle_autorisaties = validated_attrs.get(
+            "heeft_alle_autorisaties", heeft_alle_autorisaties_obj
+        )
 
         if autorisaties and heeft_alle_autorisaties is True:
             raise serializers.ValidationError(
-                _('Either autorisaties or heeft_alle_autorisaties can be specified'),
-                code='ambiguous-authorizations-specified')
+                _("Either autorisaties or heeft_alle_autorisaties can be specified"),
+                code="ambiguous-authorizations-specified",
+            )
 
         if not autorisaties and heeft_alle_autorisaties is not True:
             raise serializers.ValidationError(
-                _('Either autorisaties or heeft_alle_autorisaties should be specified'),
-                code='missing-authorizations')
+                _("Either autorisaties or heeft_alle_autorisaties should be specified"),
+                code="missing-authorizations",
+            )
 
         return validated_attrs
 
     @transaction.atomic
     def create(self, validated_data):
-        autorisaties_data = validated_data.pop('autorisaties', None)
+        autorisaties_data = validated_data.pop("autorisaties", None)
         applicatie = super().create(validated_data)
 
         if autorisaties_data:
@@ -132,11 +125,11 @@ class ApplicatieSerializer(serializers.HyperlinkedModelSerializer):
 
     @transaction.atomic
     def update(self, instance, validated_data):
-        autorisaties_data = validated_data.pop('autorisaties', None)
+        autorisaties_data = validated_data.pop("autorisaties", None)
         applicatie = super().update(instance, validated_data)
 
         # in case of update autorisaties - remove all related autorisaties
-        if autorisaties_data:
+        if autorisaties_data is not None:
             applicatie.autorisaties.all().delete()
             for auth in autorisaties_data:
                 Autorisatie.objects.create(**auth, applicatie=applicatie)
@@ -149,5 +142,6 @@ class ApplicatieUuidSerializer(ApplicatieSerializer):
     Serializer for saving data in local auth DB
     uuid is used for synchronizing identifiers with AC DB
     """
+
     class Meta(ApplicatieSerializer.Meta):
-        fields = ApplicatieSerializer.Meta.fields + ('uuid', )
+        fields = ApplicatieSerializer.Meta.fields + ("uuid",)
