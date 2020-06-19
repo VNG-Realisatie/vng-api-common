f667adf9cf3bed776621903552e44e88230a70df
Steven Bal <steven@maykinmedia.nl>
2020-06-19 11:08:05 +0200

    Added custom inspector for LengthHyperlinkedRelatedField

--- vng_api_common/inspectors/fields.py
+++ vng_api_common/inspectors/fields.py
@@ -5,7 +5,7 @@ from drf_yasg.inspectors.base import NotHandled
 from drf_yasg.inspectors.field import FieldInspector, InlineSerializerInspector
 from rest_framework import serializers
 
-from ..serializers import GegevensGroepSerializer
+from ..serializers import GegevensGroepSerializer, LengthHyperlinkedRelatedField
 
 logger = logging.getLogger(__name__)
 
@@ -93,6 +93,31 @@ class HyperlinkedIdentityFieldInspector(FieldInspector):
                 min_length=1,
                 max_length=1000,
                 description="URL-referentie naar dit object. Dit is de unieke identificatie en locatie van dit object.",
+            )
+
+        return NotHandled
+
+
+class HyperlinkedRelatedFieldInspector(FieldInspector):
+    def field_to_swagger_object(
+        self, field, swagger_object_type, use_references, **kwargs
+    ):
+        SwaggerType, ChildSwaggerType = self._get_partial_types(
+            field, swagger_object_type, use_references, **kwargs
+        )
+
+        if (
+            isinstance(field, LengthHyperlinkedRelatedField)
+            and swagger_object_type == openapi.Schema
+        ):
+            max_length = getattr(field, "max_length", None)
+            min_length = getattr(field, "min_length", None)
+            return SwaggerType(
+                type=openapi.TYPE_STRING,
+                format=openapi.FORMAT_URI,
+                min_length=min_length,
+                max_length=max_length,
+                description=field.help_text,
             )
 
         return NotHandled
