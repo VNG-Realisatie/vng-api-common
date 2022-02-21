31a5ff666e8497172a440ba381a4cba7890456cb
Sergei Maertens <sergei@maykinmedia.nl>
2022-02-21 23:33:12 +0100

    :art: Clean up test code
    Initializing middleware without get_response is removed in Django 4.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.urls import clear_script_prefix, set_script_prefix
 
 import pytest
@@ -7,3 +9,15 @@ import pytest
 def script_path(request):
     set_script_prefix("/some-prefix")
     request.addfinalizer(clear_script_prefix)
+
+
+def dummy_get_response(request):
+    raise NotImplementedError()
+
+
+@pytest.fixture()
+def request_with_middleware(rf):
+    request = rf.get("/")
+    SessionMiddleware(get_response=dummy_get_response).process_request(request)
+    MessageMiddleware(get_response=dummy_get_response).process_request(request)
+    return request
