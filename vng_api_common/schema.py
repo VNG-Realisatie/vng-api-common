b2b69319faa816bc6e6d58fbba40590254d92510
matthijsbekendam <matthijsbekendam@gmail.com>
2022-08-31 17:46:51 +0200

    custom generator for description

--- vng_api_common/schema.py
+++ vng_api_common/schema.py
@@ -1,10 +1,14 @@
 import logging
+
+from drf_spectacular.renderers import OpenApiYamlRenderer, OpenApiJsonRenderer2, OpenApiJsonRenderer, \
+    OpenApiYamlRenderer2
 from rest_framework.reverse import reverse
 
 from drf_spectacular.plumbing import get_relative_url
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 logger = logging.getLogger(__name__)
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view,OpenApiResponse
 
 
 class SchemaViewRedoc(SpectacularRedocView):
@@ -12,6 +16,17 @@ class SchemaViewRedoc(SpectacularRedocView):
         schema_url = self.url or get_relative_url(reverse(self.url_name, request=request))
         return f"{schema_url}openapi.yaml"
 
-
 class SchemaViewAPI(SpectacularAPIView):
     pass
+    # def get_renderers(self):
+    #     """
+    #     Instantiates and returns the list of renderers that this view can use.
+    #     """
+    #     if ".yaml" in self.request.path:
+    #         self.renderer_classes = [OpenApiYamlRenderer, OpenApiYamlRenderer2]
+    #     elif ".json" in self.request.path:
+    #         self.renderer_classes = [OpenApiJsonRenderer, OpenApiJsonRenderer2]
+    #
+    #     return [renderer() for renderer in self.renderer_classes]
+
+
