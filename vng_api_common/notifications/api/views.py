5643f0a3537cc3bc48bb420c91251316a2507530
Sonny Bakker <sonny@maykinmedia.nl>
2022-09-08 16:45:27 +0200

    Update attribute name

--- vng_api_common/notifications/api/views.py
+++ vng_api_common/notifications/api/views.py
@@ -17,7 +17,7 @@ class NotificationBaseView(APIView):
     Abstract view to receive webhooks
     """
 
-    swagger_schema = None
+    schema = None
 
     permission_classes = (AuthScopesRequired,)
     required_scopes = Scope(
