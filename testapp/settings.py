6a55966d36afcf4aa3ad01b71e6111cf8066a76a
Sonny Bakker <sonny@maykinmedia.nl>
2022-09-08 16:02:58 +0200

    Update testapp

--- testapp/settings.py
+++ testapp/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     "django.contrib.messages",
     "rest_framework",
-    "drf_yasg",
+    "drf_spectacular",
     "vng_api_common",
     "vng_api_common.authorizations",
     "vng_api_common.notifications",
--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,3 @@
-from vng_api_common.conf.api import BASE_SWAGGER_SETTINGS
 from vng_api_common.notifications.api.views import NotificationView as _NotificationView
 
 
@@ -6,5 +5,3 @@ class NotificationView(_NotificationView):
     """
     Ontvang notificaties via webhook
     """
-
-    swagger_schema = BASE_SWAGGER_SETTINGS["DEFAULT_AUTO_SCHEMA_CLASS"]
