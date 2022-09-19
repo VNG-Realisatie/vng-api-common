2b721c8d96c883ceb318976a2ec5fcce464c3b02
Sonny Bakker <sonny@maykinmedia.nl>
2022-09-19 16:55:50 +0200

    [#210] move `has_geo_fields` function

--- vng_api_common/inspectors/utils.py
+++ vng_api_common/inspectors/utils.py
@@ -2,6 +2,7 @@ from typing import Optional, Type
 
 from django.db import models
 
+from rest_framework import serializers
 from rest_framework.utils.model_meta import get_field_info
 
 
@@ -38,3 +39,29 @@ def get_target_field(model: Type[models.Model], field: str) -> Optional[models.F
         return get_target_field(relation_info.related_model, "__".join(remaining))
 
     return None
+
+
+def has_geo_fields(serializer) -> bool:
+    """
+    Check if any of the serializer fields are a GeometryField.
+    If the serializer has nested serializers, a depth-first search is done
+    to check if the nested serializers has `GeometryField`\ s.
+    """
+    try:
+        from rest_framework_gis.fields import GeometryField
+    except ImportError:
+        return False
+
+    for field in serializer.fields.values():
+        if isinstance(field, serializers.Serializer):
+            has_nested_geo_fields = has_geo_fields(field)
+            if has_nested_geo_fields:
+                return True
+
+        elif isinstance(field, (serializers.ListSerializer, serializers.ListField)):
+            field = field.child
+
+        if isinstance(field, GeometryField):
+            return True
+
+    return False
