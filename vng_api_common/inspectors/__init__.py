958092b69b300684b0fc324430f646ac46f6f5e3
Sonny Bakker <sonny@maykinmedia.nl>
2022-09-20 15:07:14 +0200

    [#210] remove extensions from `__init__.py`
    These should be imported wherever necessary in the API's themselfs

--- vng_api_common/inspectors/__init__.py
+++ vng_api_common/inspectors/__init__.py
@@ -1,10 +1,1 @@
-from .fields import (
-    GegevensGroepExtension,
-    HyperlinkedIdentityFieldExtension,
-    HyperlinkedRelatedFieldExtension,
-    ReadOnlyFieldExtension,
-)
-from .files import FileFieldExtension
-from .geojson import GeometryFieldExtension
-from .polymorphic import PolymorphicSerializerExtension
-from .query import FilterExtension
+
