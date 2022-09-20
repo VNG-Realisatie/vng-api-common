efffa78708ae41f3b876057f538355d3e68d7a6f
Sonny Bakker <sonny@maykinmedia.nl>
2022-09-20 15:09:31 +0200

    [#210] add comments above `FileFieldExtension`

--- vng_api_common/inspectors/files.py
+++ vng_api_common/inspectors/files.py
@@ -5,6 +5,10 @@ from drf_spectacular.plumbing import build_basic_type
 from drf_spectacular.types import OpenApiTypes
 
 
+# Note that some API's may require a seperate request/response schema in order
+# to properly display this in the schema.
+# See https://drf-spectacular.readthedocs.io/en/latest/client_generation.html?highlight=COMPONENT_SPLIT_REQUEST#component-issues
+# and https://github.com/tfranzel/drf-spectacular/issues/778
 class FileFieldExtension(OpenApiSerializerFieldExtension):
     target_class = "drf_extra_fields.fields.Base64FileField"
     match_subclasses = True
