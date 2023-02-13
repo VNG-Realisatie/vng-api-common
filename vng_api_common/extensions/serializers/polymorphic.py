baa909d59c2d4e9ae059c2acaee13dcaf6277720
matthijsbekendam <matthijsbekendam@gmail.com>
2023-02-13 12:17:25 +0100

    add mapping to discriminator and make unique names for underlying mapping values

--- vng_api_common/extensions/serializers/polymorphic.py
+++ vng_api_common/extensions/serializers/polymorphic.py
@@ -12,25 +12,29 @@ class PolymorphicSerializerExtension(OpenApiSerializerExtension):
         serializer = self.target
         schema = auto_schema._map_basic_serializer(serializer, direction)
         root_component = auto_schema.resolve_serializer(serializer, direction)
+        mapping = {}
         for attr, model_serializer in serializer.discriminator.mapping.items():
             linked_schema = {"allOf": [root_component.ref]}
-
+            root_component_name = linked_schema['allOf'][0]['$ref'].split('/')[-1]
             if model_serializer:
                 component = auto_schema.resolve_serializer(model_serializer, direction)
 
                 if component:
                     linked_schema["allOf"].append(component.ref)
 
-                linked_component = ResolvedComponent(
-                    name=attr, type=ResolvedComponent.SCHEMA, schema=linked_schema
-                )
-                auto_schema.registry.register_on_missing(linked_component)
+            mapping[attr] = f"#/components/schemas/{attr}_{root_component_name}"
+            linked_component = ResolvedComponent(
+                name=f"{attr}_{root_component_name}", type=ResolvedComponent.SCHEMA, schema=linked_schema
+            )
+
+            auto_schema.registry.register_on_missing(linked_component)
 
         polymorphic_schema = {
             "discriminator": {
                 "propertyName": underscore_to_camel(
-                    serializer.discriminator.group_field
-                )
+                    serializer.discriminator.discriminator_field
+                ),
+                "mapping":mapping
             }
         }
 
