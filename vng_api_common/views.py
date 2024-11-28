b347b2a84419d6685d841ecf8ebdb7822a8f6939
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-11-28 15:54:01 +0100

    :green_heart: [#29] clean after rebase

--- vng_api_common/views.py
+++ vng_api_common/views.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from collections import OrderedDict
 from typing import Optional
@@ -14,7 +15,7 @@ from rest_framework import exceptions as drf_exceptions, status
 from rest_framework.response import Response
 from rest_framework.views import exception_handler as drf_exception_handler
 
-from vng_api_common.client import Client, ClientError
+from vng_api_common.client import Client
 
 from . import exceptions
 from .compat import sentry_client
