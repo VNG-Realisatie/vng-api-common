403494178746fba882208ee7e49f9dd6a2c6c5f6
SonnyBA <SonnyBA@users.noreply.github.com>
2024-11-29 15:47:08 +0100

    Move zgw-consumers-oas import to related function (#50)

--- vng_api_common/tests/schema.py
+++ vng_api_common/tests/schema.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 import yaml
 from requests_mock import Mocker
-from zgw_consumers_oas.schema_loading import read_schema
 
 DEFAULT_PATH_PARAMETERS = {"version": "1"}
 
@@ -76,6 +75,8 @@ def get_validation_errors(response, field, index=0):
 def mock_service_oas_get(
     mock: Mocker, url: str, service: str, oas_url: str = ""
 ) -> None:
+    from zgw_consumers_oas.schema_loading import read_schema
+
     if not oas_url:
         oas_url = f"{url}schema/openapi.yaml?v=3"
 
