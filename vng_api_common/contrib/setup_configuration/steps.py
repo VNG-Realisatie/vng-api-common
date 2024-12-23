f093d9d5bba5210891f247282cbb942142d82ad7
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-12-23 11:37:09 +0100

    :sparkles: add configuration step for Applicatie model

--- vng_api_common/contrib/setup_configuration/steps.py
+++ vng_api_common/contrib/setup_configuration/steps.py
@@ -1,8 +1,9 @@
 from django_setup_configuration.configuration import BaseConfigurationStep
 
+from vng_api_common.authorizations.models import Applicatie
 from vng_api_common.models import JWTSecret
 
-from .models import JWTSecretsConfigurationModel
+from .models import ApplicatieConfigurationModel, JWTSecretsConfigurationModel
 
 
 class JWTSecretsConfigurationStep(BaseConfigurationStep[JWTSecretsConfigurationModel]):
@@ -20,4 +21,26 @@ class JWTSecretsConfigurationStep(BaseConfigurationStep[JWTSecretsConfigurationM
             JWTSecret.objects.update_or_create(
                 identifier=config.identifier,
                 defaults={"secret": config.secret},
+            )
+
+
+class ApplicatieConfigurationStep(BaseConfigurationStep[ApplicatieConfigurationModel]):
+    """
+    Configure Applicatie used for authorization
+    """
+
+    verbose_name = "Configuration to create applicaties"
+    config_model = ApplicatieConfigurationModel
+    namespace = "vng_api_common_applicaties"
+    enable_setting = "vng_api_common_applicaties_config_enable"
+
+    def execute(self, model: ApplicatieConfigurationModel):
+        for config in model.items:
+            Applicatie.objects.update_or_create(
+                uuid=config.uuid,
+                defaults={
+                    "client_ids": config.client_ids,
+                    "label": config.label,
+                    "heeft_alle_autorisaties": config.heeft_alle_autorisaties,
+                },
             )
